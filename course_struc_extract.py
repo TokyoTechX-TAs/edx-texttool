ced93138a2becb141b44298c6bc3107999ff655f
Nopphon Keerativoranan <ke.nopphon@gmail.com>
2017-11-04 15:49:24 +0900

    2nd commit, updated compatibility to Python3

--- course_struc_extract.py
+++ course_struc_extract.py
@@ -1,18 +1,32 @@
 # coding=utf-8
 
-import os, tarfile, shutil, xlrd,xlwt, datetime
+import os,sys, tarfile, shutil, xlrd,xlwt, datetime,logging,importlib
 from lxml import etree
 
+#importlib.reload(sys)
+#sys.setdefaultencoding('utf8')
 
 """
-	Const declaration
-	this consts will help us in case of modifications in the excel sheet
+	this script will extract course structure of exported courses into excel file
+	excel　file will be created in this directory
+	
 """
 
 """
-	sheet->Video
+	initialize index of excel sheet  
+	xlsmPath 			-> excel filename 
+	HTMLSHEET 			-> sheetname        
+	HTMLINDEX 			-> col1
+	HTMLSECTION 		-> col2
+	HTMLSUBSECTION 		-> col3
+	HTMLUNIT 			-> col4
+	HTMLCOMPNAME 		-> col5
+	HTMLLOC 			-> col6
+	HTMLFILE 			-> col7
+
 """
-HTMLSHEET = "html"
+xlsmPath = "conversion_table.xls"
+HTMLSHEET = "html"        
 HTMLINDEX = 0
 HTMLSECTION = 1
 HTMLSUBSECTION = 2
@@ -22,34 +36,50 @@ HTMLLOC = 5
 HTMLFILE = 6
 
 
-
-
-
-
-idx = 0
-
-#export_path = ""
-"""
-hardcoded xlsmpath must change to a parameter
-"""
-xlsmPath = "conversion_table.xls"
 book = xlwt.Workbook()
 sheet = book.add_sheet(HTMLSHEET)
 
-def read_course():
 
-	print "----------------generate excel file " + xlsmPath + " , sheetname " + HTMLSHEET + "----------------"
-	idx = 0
-	sheet.write(idx,0, "no")
-	sheet.write(idx,1, "section")
-	sheet.write(idx,2, "subsection")
-	sheet.write(idx,3, "unit")
-	sheet.write(idx,4, "component_name")
-	sheet.write(idx,5, "file_loc")
-	sheet.write(idx,6, "file_name")
+
+
+
+
+idx = 1 
+
+
+
+def read_course():
+	"""
+		write row title in the first row of excel sheet
+	"""
+
+	sheet.col(HTMLINDEX).width 		= 256 * 4				### col size = 4  characters-long
+	sheet.col(HTMLSECTION).width 	= 256 * 50				### col size = 50 characters-long
+	sheet.col(HTMLSUBSECTION).width = 256 * 50				### col size = 50 characters-long
+	sheet.col(HTMLUNIT).width 		= 256 * 50				### col size = 50 characters-long
+	sheet.col(HTMLCOMPNAME).width 	= 256 * 50				### col size = 50 characters-long
+	sheet.col(HTMLLOC).width 		= 256 * 50				### col size = 50 characters-long
+	sheet.col(HTMLFILE).width 		= 256 * 50				### col size = 50 characters-long
+
+	print("----------------generate excel file " + xlsmPath + " , sheetname " + HTMLSHEET + "----------------")
+	row_title_idx = 0
+	sheet.write(row_title_idx,HTMLINDEX, 		"no")
+	sheet.write(row_title_idx,HTMLSECTION, 		"section")
+	sheet.write(row_title_idx,HTMLSUBSECTION, 	"subsection")
+	sheet.write(row_title_idx,HTMLUNIT, 		"unit")
+	sheet.write(row_title_idx,HTMLCOMPNAME, 	"component_name")
+	sheet.write(row_title_idx,HTMLLOC, 			"file_loc")
+	sheet.write(row_title_idx,HTMLFILE, 		"file_name")
 	read_chapter()
 
 def read_chapter():
+	"""
+		extract section name from exported course (subfolder -> chapter)
+		param: 
+		- chapter_name -> section name
+		- seq_ls -> object of associated subsection's XML filenames
+	"""
+
 	chap_path = "course/chapter"
 	chap_ls = os.listdir(chap_path)
 	for each_chap in chap_ls:
@@ -57,12 +87,19 @@ def read_chapter():
 		root = tree.getroot()
 		chapter_name = root.get('display_name')
 		seq_ls = root.findall(".sequential")
-		read_sequential(chapter_name,seq_ls,)
+		read_sequential(chapter_name,seq_ls)
 		
 	
 def read_sequential(_current_chap_name,_seq_ls):
+
+	"""
+		extract associated subsection names from exported course (subfolder -> sequential)
+		param:
+		- seq_name -> associated subsection name
+		- ver_ls -> object of associated unit's XML filenames
+	"""
+
 	seq_path = "course/sequential"
-	#seq_ls = os.listdir(seq_path)
 	for each_seq in _seq_ls:
 		seq_filename = each_seq.get('url_name') + ".xml"
 		tree = etree.parse(seq_path+"/"+seq_filename)
@@ -72,9 +109,15 @@ def read_sequential(_current_chap_name,_seq_ls):
 		read_vertical(_current_chap_name,seq_name,ver_ls)
 
 def read_vertical(_current_chap_name,_current_seq_name,_ver_ls):
-	ver_path = "course/vertical"
 
-	#seq_ls = os.listdir(seq_path)
+	"""
+		extract associated unit names from exported course (subfolder -> vertical)
+		param: 
+		- ver_name -> associated unit name
+		- html_ls -> object of associated html_component's XML filenames
+	"""
+
+	ver_path = "course/vertical"
 	for each_ver in _ver_ls:
 		ver_filename = each_ver.get('url_name') + ".xml"
 		tree = etree.parse(ver_path+"/"+ver_filename)
@@ -83,47 +126,73 @@ def read_vertical(_current_chap_name,_current_seq_name,_ver_ls):
 		html_ls = root.findall(".html")
 		read_html(_current_chap_name,_current_seq_name,ver_name,html_ls)
 		#print(html_ls)
-		#read_vertical(_current_chap_name,seq_name,ver_ls)
+		
 
 
 def read_html(_current_chap_name,_current_seq_name,_current_ver_name,_html_ls):
-	html_path = "course/html"
 
-	#seq_ls = os.listdir(seq_path)
+	"""
+		extract associated html names from exported course (subfolder -> html)
+		param: 
+		- html_name -> associated html name
+		
+	"""
+	html_path = "course/html"
 	for each_html in _html_ls:
 		html_filename = each_html.get('url_name') + ".xml"
 		tree = etree.parse(html_path+"/"+html_filename)
 		root = tree.getroot()
 		html_name = root.get('display_name')
-		if html_name == "Licensing":
+	
+		#### this condition avoid listing 'licensing' HTML component.  #####
+		
+		if html_name == "Licensing":         
 			continue
+
+		######################################################################
+
 		add_course_struc(_current_chap_name,_current_seq_name,_current_ver_name,html_name)
 
 def add_course_struc(_current_chap_name,_current_seq_name,_current_ver_name,_current_html_name):
-
-	#print type(row_count),type(_current_chap_name.encode('ascii','ignore')),type(_current_seq_name.encode('ascii','ignore')),type(_current_ver_name.encode('ascii','ignore')),type(_current_html_name.encode('ascii','ignore'))
+	"""
+		write course structure in each row of excel sheet 
+		param:
+		idx 				-> row_index
+		section_name 		-> _current_chap_name
+		subsection_name		-> _current_seq_name
+		unit_name			-> _current_ver_name
+		component_name		-> _current_html_name
+	"""
+	
 	global idx 
-	idx +=1
-	#print row_count
+	
 	sheet.write(idx,0, idx)
 	sheet.write(idx,1, _current_chap_name)
 	sheet.write(idx,2, _current_seq_name)
 	sheet.write(idx,3, _current_ver_name)
 	sheet.write(idx,4, _current_html_name)
 	if _current_html_name is None:
-		print "row " + str(idx) +" : text component with 'No title' " + "is copied"
+		print("row " + str(idx) +" : text component with 'No title' " + "is extracted")
 	else:
-		print "row " + str(idx) +" : text component: '" + _current_html_name +"' is copied"
+		print("row " + str(idx) +" : text component: '" + str(_current_html_name) +"' is extracted")
+	idx +=1
 	
 	
 
 
-def generate_Edx():
+def main():
 	
-
-	read_course()
-	book.save(xlsmPath)
-
+	read_course()                   #### course extraction
+	book.save(xlsmPath)				#### save excel file 
 
 
-generate_Edx()
+if __name__ == '__main__':
+	try:
+		main()
+		closeInput = input("\nPress ENTER to exit")
+		print("Closing...")
+
+	except KeyboardInterrupt:
+		logging.warn("\n\nCTRL-C detected, shutting down....")
+		sys.exit(ExitCode.OK)
+
--- text_replacement.py
+++ text_replacement.py
@@ -6,10 +6,28 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 
 
-reload(sys)
-sys.setdefaultencoding('utf8')
+#reload(sys)
+#sys.setdefaultencoding('utf8')
+"""
+	this script will replace HTML component to exported courses according to excel file
+	
+"""
 
+"""
+	initialize index of excel sheet  
+	xlsmPath 			-> excel filename 
+	HTMLSHEET 			-> sheetname        
+	HTMLINDEX 			-> col1
+	HTMLSECTION 		-> col2
+	HTMLSUBSECTION 		-> col3
+	HTMLUNIT 			-> col4
+	HTMLCOMPNAME 		-> col5
+	HTMLLOC 			-> col6
+	HTMLFILE 			-> col7
 
+"""
+
+xlsmPath = "conversion_table.xls"
 HTMLSHEET = "html"
 HTMLINDEX = 0
 HTMLSECTION = 1
@@ -19,45 +37,84 @@ HTMLCOMPNAME = 4
 HTMLLOC = 5
 HTMLFILE = 6
 
+"""
+	initialize path  
 
+	path 				-> course folder
+	source_path 		-> source folder        
+	backup_souce_path 	-> original course folder
+	
+
+"""
 
 
 global path,source_path
 path = "course"
 source_path = "source"
 backup_souce_path = "original_course"
-xlsmPath = "conversion_table.xls"
+
 wb = xlrd.open_workbook(xlsmPath)
 
+
+"""
+	create error log file as 'error_files.txt'
+"""
 file_err = open('error_files.txt', 'a') 
 txt = "starttime" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') +"\n"
 file_err.write(txt)
 file_err.close()
 
 
-def read_find_html():
-	print "---------------------------------begin reading html metadat from excel file----------------------------"
 
+def read_find_html():
+
+	"""
+		1. open excel file
+		2. begin reading course structure from excel's rows [section_name,subsection_name,unit_name,component_name]
+				
+	"""
+
+	print("---------------------------------begin reading html metadata from excel file----------------------------")
 
 	global sheetstruc
 	sheetstruc = wb.sheet_by_name(HTMLSHEET)
 	for row in range(1, sheetstruc.nrows):
 
 
-		chapter_name = sheetstruc.cell_value(row,HTMLSECTION)
-		seq_name = sheetstruc.cell_value(row,HTMLSUBSECTION)
-		ver_name = sheetstruc.cell_value(row,HTMLUNIT)
-		comp_name = sheetstruc.cell_value(row,HTMLCOMPNAME)
-		print "-------------------- begin replacing HTML component < " + comp_name + " > --------------------------------" 
-		#print type(comp_name)
+		chapter_name 	= sheetstruc.cell_value(row,HTMLSECTION)
+		seq_name 		= sheetstruc.cell_value(row,HTMLSUBSECTION)
+		ver_name 		= sheetstruc.cell_value(row,HTMLUNIT)
+		comp_name 		= sheetstruc.cell_value(row,HTMLCOMPNAME)
+
+		if (len(sheetstruc.cell_value(row,HTMLLOC)) == 0) or (len(sheetstruc.cell_value(row,HTMLFILE)) == 0):
+
+			print("--------------- no translated text file & directory :skipped HTML component  < " + str(comp_name) + " > -------------------")
+			continue
+
+		print("-------------------- begin replacing HTML component < " + str(comp_name) + " > --------------------------------")
+
+
 		map_html_chapter(row,chapter_name)
-		print "-------------------------------------------------------------------------------------------------------"+ "\n"
+
+
+
+		print("-------------------------------------------------------------------------------------------------------"+ "\n")
 		
 		
 		
 		
 
 def map_html_chapter(_row,_chapter_name):
+
+	"""
+		1. seach location of section_name listed from excel at exported course (subfolder -> chapter)
+			2.1 if found, search subsection
+			2.2 if not found, stop script
+		param:
+		current_chapter_name -> section_name
+		seq_ls 				 ->  object of associated subsection's XML filenames
+				
+	"""
 
 	current_chapter_name = _chapter_name.rstrip().replace(u'\xa0', ' ')
 	chap_path = path + "/chapter"
@@ -66,14 +123,26 @@ def map_html_chapter(_row,_chapter_name):
 		tree = etree.parse(chap_path+"/"+each_chap)
 		root = tree.getroot()
 		if current_chapter_name.lower() == root.get('display_name').lower():
-			print "Found Sections: <" + str(current_chapter_name) + "> at filename --> " + str(each_chap)
+			print("Found Sections: <" + str(current_chapter_name) + "> at filename --> " + str(each_chap))
 			seq_ls = root.findall(".sequential")
 			map_html_seq(seq_ls,_row)
 			return()
-	print "could not find Section: <" + (current_chapter_name) + "> Please check Section name on excel again" 
+	print("could not find Section: <" + (current_chapter_name) + "> Please check Section name on excel again")
 	quit()
 
 def map_html_seq(_current_seq_list,_row):
+
+	"""
+		1. seach location of associated subsection_name listed from excel at exported course (subfolder -> sequential)
+			2.1 if found, search unit
+			2.2 if not found, stop script
+		
+		param:
+		current_seq_name -> subsection_name
+		ver_ls 			 ->  object of associated unit's XML filenames
+				
+	"""
+
 
 	current_seq_name = sheetstruc.cell_value(_row,HTMLSUBSECTION).rstrip().replace(u'\xa0', ' ')
 	seq_path = path +"/sequential"
@@ -82,15 +151,27 @@ def map_html_seq(_current_seq_list,_row):
 		tree = etree.parse(seq_path+"/"+seq_filename)
 		root = tree.getroot()
 		if current_seq_name.lower() == root.get('display_name').lower():
-			print "Found Subsections: <" + str(current_seq_name) + "> at filename --> " + str(each_seq.get('url_name'))
+			print("Found Subsections: <" + str(current_seq_name) + "> at filename --> " + str(each_seq.get('url_name')))
 			ver_ls = root.findall(".vertical")
 			map_html_ver(ver_ls,_row)
 			return()
-	print "could not find Section: <" + (current_seq_name) + "> Please check Subsection name on excel again" 
+	print("could not find Section: <" + (current_seq_name) + "> Please check Subsection name on excel again")
 	quit()
 
 
 def map_html_ver(_current_ver_list,_row):
+
+	"""
+		1. seach location of associated unit_name listed from excel at exported course (subfolder -> vertical)
+			2.1 if found, search html component
+			2.2 if not found, stop script
+		
+		param:
+		current_ver_name -> unit_name
+		html_ls 			 ->  object of associated html_component's XML filenames
+				
+	"""
+
 	
 
 	ver_path = path +"/vertical"
@@ -100,16 +181,30 @@ def map_html_ver(_current_ver_list,_row):
 		tree = etree.parse(ver_path+"/"+ver_filename)
 		root = tree.getroot()
 		if current_ver_name.lower() == root.get('display_name').lower():
-			print "Found Units: <" + str(current_ver_name) + "> at filename --> " + str(each_ver.get('url_name'))
+			print("Found Units: <" + str(current_ver_name) + "> at filename --> " + str(each_ver.get('url_name')))
 			html_ls = root.findall(".html")
 			map_html_component(html_ls,_row)
 			return()
 
-	print "could not find Unit: <" + (current_ver_name) + "> Please check Unit name on excel again" 
+	print("could not find Unit: <" + str(current_ver_name) + "> Please check Unit name on excel again") 
 	quit()
 
 
 def map_html_component(_current_html_list,_row):
+
+
+	"""
+		1. seach location of text_component_name listed from excel at exported course (subfolder -> html)
+			2.1 if found, start replacing HTML file
+			2.2 if not found, stop script
+		
+		param:
+		current_html_name -> html_name
+		html_link 		  -> original HTML file of text
+				
+	"""
+
+
 	html_path = path +"/html"
 	current_html_name = sheetstruc.cell_value(_row,HTMLCOMPNAME).rstrip().replace(u'\xa0', ' ')
 
@@ -125,14 +220,26 @@ def map_html_component(_current_html_list,_row):
 			each_htmlname	 = root.get('display_name')
 
 		if current_html_name.lower() == each_htmlname.lower():
-			print "index number "+ str(sheetstruc.cell_value(_row,HTMLINDEX)) + " Found HTML: <" + (current_html_name) + "> at filename --> " + html_filename
+			print("index number "+ str(sheetstruc.cell_value(_row,HTMLINDEX)) + " Found HTML: <" + str(current_html_name) + "> at filename --> " + str(html_filename))
 			replace_content(html_link,_row)
 			return()
 
-	print "index number "+ str(sheetstruc.cell_value(_row,HTMLINDEX)) +" could not find HTML Component: <" + (current_html_name) + "> Please check HTML name on excel again" 
+	print("index number "+ str(sheetstruc.cell_value(_row,HTMLINDEX)) +" could not find HTML Component: <" + str(current_html_name) + "> Please check HTML name on excel again")
 	quit()
 
 def replace_content(_filename,_row):
+
+
+	"""
+		1. locate translated HTML file in local directory listed on excel file
+		2. replace HTML file in exported course with (1)
+		
+		param:
+		source_loc  -> location of translated text in HTML file 
+		source_file -> location of original text in HTML file 
+				
+	"""
+
 
 	source_loc = str(sheetstruc.cell_value(_row,HTMLLOC))
 	source = str(sheetstruc.cell_value(_row,HTMLFILE))
@@ -143,7 +250,7 @@ def replace_content(_filename,_row):
 	des_file = html_path+"/"+_filename + ".html"
 	shutil.copyfile(source_file, des_file)
 
-	modify_figure_src(des_file,_filename)
+	modify_figure_src(des_file,_filename,_row)
 	
 	
 
@@ -151,11 +258,26 @@ def replace_content(_filename,_row):
 
 
 
-	print "-------------------- Eng-to-Jap text replacing is complete --------------------------------" 
+	print("-------------------- Eng-to-Jap text replacing is complete --------------------------------") 
 
 
 
-def modify_figure_src(_des_path,_filename):
+def modify_figure_src(_des_path,_filename,_row):
+
+
+	"""
+		changing figure's source in HTMl file
+		param:
+		img_tag_eng -> original figure's source from original HTML file
+		img_tag_translated　-> translated ver. figure's source from translated text HTML file
+
+		beware:
+		this script requires number of figure and order are the same as original HTML file.
+		Otherwise, figure's source will not be changed, thus not shown in edx course 
+		
+				
+	"""
+
 
 	backup_path = path +"/html"
 	backup_file = backup_souce_path+"/"+backup_path+"/"+_filename + ".html"
@@ -167,56 +289,67 @@ def modify_figure_src(_des_path,_filename):
 	img_tag_eng = tag_eng.find_all('img')
 	file_eng.close()
 
-  	file_jap = open(_des_path, 'r') 
-	jap_text = file_jap.read() 
-	tag_jap = BeautifulSoup(jap_text, 'html.parser')
-	img_tag_jap = tag_jap.find_all('img')
-	file_jap.close()
+	file_translated = open(_des_path, 'r') 
+	translated_text = file_translated.read() 
+	tag_translated = BeautifulSoup(translated_text, 'html.parser')
+	img_tag_translated = tag_translated.find_all('img')
+	file_translated.close()
 
 
 	if len(img_tag_eng) != 0:
-		if len(img_tag_jap) == len(img_tag_eng):
+		if len(img_tag_translated) == len(img_tag_eng):
 
-			for i in range(len(img_tag_jap)):
-				img_tag_jap[i].attrs['src'] =img_tag_eng[i].attrs['src']
-			jap_text = tag_jap.prettify()
-			file_jap_mod = open(_des_path, 'w') 
-			file_jap_mod.write(jap_text) 
-			file_jap_mod.close()
-			print "figure sources are all modified"
-
-
-
+			for i in range(len(img_tag_translated)):
+				img_tag_translated[i].attrs['src'] =img_tag_eng[i].attrs['src']
+			translated_text = tag_translated.prettify()
+			file_translated_mod = open(_des_path, 'w') 
+			file_translated_mod.write(translated_text) 
+			file_translated_mod.close()
+			print("figure sources are all modified")
 
 		else:
-			print "number of figure is not equal to the source"
+			print("number of figure is not equal to the source")
 			file_err = open('error_files.txt', 'a') 
-			txt = "fig in Jap text,fig in Eng text, path = " + str(len(img_tag_jap)) + " , " + str(len(img_tag_eng)) +" , " +_des_path + "\n"
+			txt = "row in excel = " + str(sheetstruc.cell_value(_row,HTMLINDEX)) +"\n"
+			txt = txt + "--> fig in translated text = "+  str(len(img_tag_translated)) + " ,fig in original text = "  +str(len(img_tag_eng))  +"\n"
+			txt = txt + "----------------------------------------------\n"
 			file_err.write(txt)
 			file_err.close()
 
 
 	else:
-		print "no-figure text content"
+		print("no-figure text content")
 
 	
 def make_tarfile():
 
-	print "file is being compressed as tar.gz "
+	"""
+		compress exported course into tar.gz	
+	"""
+
+	print("file is being compressed as tar.gz ")
 	with tarfile.open(path + '/' + path + '.tar.gz', 'w:gz') as tar:
 		for f in os.listdir(path):
 			tar.add(path + "/" + f, arcname=os.path.basename(f))
 		tar.close()
-	print "uploadable file is created at " + path + '/' + path + '.tar.gz'
+	print("uploadable file is created at " + path + '/' + path + '.tar.gz')
 
 
 
 
-def generate_Edx():
+def main():
 	
 	read_find_html()
 	make_tarfile()
 
 
+if __name__ == '__main__':
+	try:
+		main()
+		closeInput = input("\nPress ENTER to exit")
+		print ("Closing...")
 
-generate_Edx()
+	except KeyboardInterrupt:
+		logging.warn("\n\nCTRL-C detected, shutting down....")
+		sys.exit(ExitCode.OK)
+
